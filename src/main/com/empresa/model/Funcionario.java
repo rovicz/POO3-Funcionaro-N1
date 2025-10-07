51d3f1e32f2140054c6f005af1ec3c1ef13d1123
rovicz <roviczzf@gmail.com>
2025-10-06 21:29:10 -0300

    dev: creating funcionario model at /empresa/model

---
+++ src/main/com/empresa/model/Funcionario.java
@@ -0,0 +1,49 @@
+package com.empresa.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario {
+    private String matricula;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String cargo;
+    private BigDecimal salario;
+    private LocalDate dataContratacao;
+    private Endereco endereco;
+
+    public Funcionario(String matricula, String nome, String cpf, LocalDate dataNascimento, String cargo, BigDecimal salario, LocalDate dataContratacao, Endereco endereco) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.dataContratacao = dataContratacao;
+        this.endereco = endereco;
+    }
+
+    // Getters e Setters
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public LocalDate getDataNascimento() { return dataNascimento; }
+    public void setDataNascimento(LocalDate dataNascimento) { this.dataNascimento = dataNascimento; }
+    public String getCargo() { return cargo; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
+    public BigDecimal getSalario() { return salario; }
+    public void setSalario(BigDecimal salario) { this.salario = salario; }
+    public LocalDate getDataContratacao() { return dataContratacao; }
+    public void setDataContratacao(LocalDate dataContratacao) { this.dataContratacao = dataContratacao; }
+    public Endereco getEndereco() { return endereco; }
+    public void setEndereco(Endereco endereco) { this.endereco = endereco; }
+
+    @Override
+    public String toString() {
+        return String.join(";", matricula, nome, cpf, dataNascimento.toString(), cargo, salario.toString(), dataContratacao.toString(), endereco.toString());
+    }
+}
